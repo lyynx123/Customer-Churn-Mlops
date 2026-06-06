25ceb77932c1c728a709d421a30337caa232dae9
lyynx123 <Ahmzudin@gmail.com>
2026-06-06 16:45:58 +0700

    Create modelling.py

---
+++ MLProject/modelling.py
@@ -0,0 +1,36 @@
+import dagshub
+import mlflow
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+
+def train_model():
+    # Menghubungkan tracking ke DagsHub online (Kriteria 2 Advance)
+    dagshub.init(repo_owner='lyynx123', repo_name='Eksperimen_SML_Ahmad', mlflow=True)
+    
+    # Membaca dataset
+    df = pd.read_csv('MLProject/Customer_Churn_Dataset_preprocessing.csv')
+    X = df.drop(columns=['Churn'])
+    y = df['Churn']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+    
+    mlflow.set_experiment("Telco_Churn_RandomForest")
+    
+    with mlflow.start_run(run_name="CI_Automated_Run"):
+        model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+        model.fit(X_train, y_train)
+        
+        y_pred = model.predict(X_test)
+        
+        mlflow.log_param("n_estimators", 100)
+        mlflow.log_param("max_depth", 10)
+        mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+        mlflow.log_metric("f1_score", f1_score(y_test, y_pred))
+        
+        mlflow.sklearn.log_model(model, "random_forest_model")
+        print("Model Automated Training via CI Berhasil!")
+
+if __name__ == "__main__":
+    train_model()

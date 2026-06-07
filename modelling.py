402eadf64e99a1121a504e99bafaaec0791bf9a7
lyynx123 <Ahmzudin@gmail.com>
2026-06-07 10:09:02 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -1,32 +1,36 @@
+import dagshub
 import mlflow
+import mlflow.sklearn
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def train_model():
-    # Membaca dataset terupdate yang berada di halaman root depan
+    # Inisialisasi DagsHub
+    dagshub.init(repo_owner='lyynx123', repo_name='Eksperimen_SML_Ahmad', mlflow=True)
+    
+    # Load Data bersih
     df = pd.read_csv('Customer_Churn_Dataset_preprocessing.csv')
     X = df.drop(columns=['Churn'])
     y = df['Churn']
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
     
-    # Pelatihan Model Utama
-    model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    # Set nama eksperimen
+    mlflow.set_experiment("Telco_Churn_RandomForest")
     
-    # LANGSUNG LOGGING (Tanpa 'with mlflow.start_run()')
-    # Karena MLflow Project otomatis menyediakan active run di latar belakang
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_param("max_depth", 10)
-    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
-    mlflow.log_metric("f1_score", f1_score(y_test, y_pred))
+    # --- PERBAIKAN WAJIB: Mengaktifkan Autolog Sebelum Training ---
+    mlflow.sklearn.autolog()
     
-    # Log model utama agar bisa dibungkus menjadi Docker Image
-    mlflow.sklearn.log_model(model, "random_forest_model")
-    print("Model Automated Training via MLflow Project Berhasil!")
+    with mlflow.start_run(run_name="RF_Production_Autolog_Run"):
+        model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+        model.fit(X_train, y_train)
+        
+        y_pred = model.predict(X_test)
+        
+        # Cetak metrik dasar untuk memastikan model berjalan baik
+        print("Model training selesai dengan Autolog.")
 
 if __name__ == "__main__":
     train_model()
